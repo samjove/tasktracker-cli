e3d3ad0a8cf7b73f32589d4edb8a710c3f819cf7
samjove <vermo7en@gmail.com>
2024-09-03 11:02:09 -0400

    Adds, saves, and loads tasks to/from json file

--- tasktracker.py
+++ tasktracker.py
@@ -1,12 +1,17 @@
 import argparse
+import os
 import sys
 import json
+from enum import Enum
+from datetime import datetime
 
+# TODO add error checking and edge case handling
+# TODO add tests
 class TaskTracker():
     def __init__(self):
         action = None
         options = None
-        
+        status = ('todo', 'done', 'in-progress')
         
     def task_parser(self):
         parser = argparse.ArgumentParser(
@@ -14,16 +19,44 @@ class TaskTracker():
             description='Adds, updates, deletes, and lists tasks.',
         )
         parser.add_argument('action', choices=['add', 'update', 'delete', 'mark-in-progress', 'mark-done', 'list'])
-        parser.add_argument('options', action='append', nargs="*")
+        parser.add_argument('options', nargs="*")
         return parser.parse_args()
     
     def exec(self):
         args = self.task_parser()
-        print(args)
+        print(args.action)
+        print(args.options[0])
+        file = 'task_list.json'
         # TODO validate args
         # TODO add JSON file functionality
+        if (args.action == 'add'):
+            self.add_task(args.options[0], file)
+    
+    def save_tasks(self, tasks, file):
+        with open(file, 'w') as f:
+            json.dump({"tasks": tasks}, f, indent=4)
+            
+    def load_tasks(self, file):
+        if os.path.exists(file):
+            with open(file, 'r') as f:
+                return json.load(f).get('tasks', [])
+        return []
         
-        
+    def add_task(self, to_do, file):
+        tasks = self.load_tasks(file)
+        new_id = max([task['id'] for task in tasks], default=0) + 1
+        task = {
+            'id': new_id,
+            'description': to_do,
+            'status': 'todo',
+            'createdAt': datetime.now().isoformat(),
+            'updatedAt': datetime.now().isoformat()
+        }
+        tasks.append(task)
+        self.save_tasks(tasks, file)
+        # TODO create task object to add into JSON
+        # TODO create id increment for tasks
+        # TODO add error and exception checking for JSON ops    
 def main():
     task_tracker = TaskTracker()
     task_tracker.exec()
